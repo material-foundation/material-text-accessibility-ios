08ab04402ac6ef608a69794bd97e6d5e86d21511
Domenic Bianchi <domenic@oncallhealth.com>
2021-02-01 15:21:10 -0500

    Adding SPM

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "MDFTextAccessibility",
+    platforms: [
+        .iOS("9.0"), .tvOS("9.0")
+    ],
+    products: [
+        .library(name: "MDFTextAccessibility", targets: ["MDFTextAccessibility"])
+    ],
+    targets: [
+        .target(
+            name: "MDFTextAccessibility",
+            path: "src",
+            publicHeadersPath: ".")
+    ]
+)
